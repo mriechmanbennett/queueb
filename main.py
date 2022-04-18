bc3d839020cb9c587cf481147ea1da094ef0b56a
Matt Riechman-Bennett <mriechmanbennett@protonmail.com>
2022-04-17 21:49:40 -0500

    added initial functionality

--- main.py
+++ main.py
@@ -3,24 +3,89 @@ import discord
 from discord.ext import commands
 import os
 
+
 # retrieves the secret key from another directory outside the repo.
 # temporary fix until I spend time to learn a better way
 def getSecret():
-    os.chdir('../secrets')
+    os.chdir('..')
     secretFile = open('secret.txt', 'r')
     fileString = secretFile.read()
     secretFile.close()
-    os.chdir('../queueb')
+    os.chdir('queueb')
     return fileString
 
+
+# function to initialize queue
+
+# function to join the queue
+
+# function to leave the queue
+
+# function to turn the lobby list into a string for printing
+def playerListStr(pQueue):
+    playerListString = ''
+    for item in pQueue:
+        playerListString += str(item) + '\n'
+    return playerListString
+
+# function to print the lobby queue
+def lobbyToString(pQueue):
+    lobbyMarkdownString = ('```Lobby Queue [' + str(len(pQueue)) + '/10]\n' + str(playerListStr(pQueue)) + '```')
+    return lobbyMarkdownString
+
+
+# function to clear the queue (admin only)
+
+
+##########################################
+# Begins Main:
+##########################################
+# Retrieves secret key from the outside file
 secret = getSecret()
 
+# Initializes the game queue and the lobby queue
+gameQueue = []
+lobbyQueue = []
+
+# Initializes the bot with the command prefix ;
+bot = commands.Bot(command_prefix=';')
 
 
-@client.event
+@bot.event
 async def on_ready():
     print('Bot is ready')
 
-@client.command()
+
+@bot.command()
 async def ping(ctx):
-    await ctx.send
+    await ctx.send('Pong!')
+
+
+# commands related to registration
+
+# commands related to user access to the queue
+@bot.command()
+async def join(ctx):
+    if ctx.author in lobbyQueue:
+        await ctx.send(str(ctx.author) + ' is already in queue')
+    else:
+        lobbyQueue.append(ctx.author)
+        await ctx.send(str(ctx.author) + ' has joined the queue')
+
+
+@bot.command()
+async def q(ctx):
+    lobbyPrint = lobbyToString(lobbyQueue)
+    await ctx.send(lobbyPrint)
+
+
+@bot.command()
+async def leave(ctx):
+    if ctx.author in lobbyQueue:
+        lobbyQueue.remove(ctx.author)
+        await ctx.send(str(ctx.author) + ' has left the queue')
+    else:
+        await ctx.send('You were not in queue')
+
+
+bot.run(secret)
